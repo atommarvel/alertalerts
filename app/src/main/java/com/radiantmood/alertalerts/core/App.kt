32135a6b33afd64cb6e49d2988be5bab200a3519
atommarvel <atom.raiff@gmail.com>
2020-02-04 23:18:05 -0800

    add stetho

--- app/src/main/java/com/radiantmood/alertalerts/core/App.kt
+++ app/src/main/java/com/radiantmood/alertalerts/core/App.kt
@@ -1,6 +1,7 @@
 package com.radiantmood.alertalerts.core
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import com.radiantmood.alertalerts.di.AppComponent
 import com.radiantmood.alertalerts.di.ContextModule
 import com.radiantmood.alertalerts.di.DaggerAppComponent
@@ -11,6 +12,7 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         initDagger()
+        Stetho.initializeWithDefaults(this)
     }
 
     private fun initDagger() {
